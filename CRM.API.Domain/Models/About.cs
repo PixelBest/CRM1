06237b59ae418323ca2daad4bca037879355085b
PixelBest <nick.poroschin@yandex.ru>
2023-05-29 17:44:10 +0500

    Добавьте файлы проекта.

---
+++ CRM.API.Domain/Models/About.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
+
+namespace CRM.API.Models
+{
+    [Table("About")]
+    public class About
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte[] Url { get; set; }
+        public string Text { get; set; }
+        public string Path { get; set; }
+    }
+}
